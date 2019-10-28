ef3acd1ab4aad816b77e8967da4edb2801ddf3bc
Carolina Beatriz Berry <carolbpr@gmail.com>
2019-10-28 06:49:05 -0500

    Updates

---
+++ precache-manifest.6126836724087222ebac23e03900ed23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a241fe22c03ebf91fbe88c91aa647d5",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8bdfc7b11461ac9a1806",
+    "url": "/clicky-game/static/css/main.2323356f.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "8bdfc7b11461ac9a1806",
+    "url": "/clicky-game/static/js/main.9017e82e.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
